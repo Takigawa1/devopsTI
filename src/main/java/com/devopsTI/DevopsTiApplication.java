454346e54ba52f88de020b0af63ef884ad5c17b6
Victor A <alonsovictor167@gmail.com>
2018-04-06 11:43:00 -0500

    Initial commit for our first Spring boot project

---
+++ src/main/java/com/devopsTI/DevopsTiApplication.java
@@ -0,0 +1,12 @@
+package com.devopsTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTiApplication.class, args);
+	}
+}
